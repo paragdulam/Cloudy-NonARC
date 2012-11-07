45f9a2ca919c4eeba85e32cb1d473ee6ea80711d
Parag Dulam <paragdulam@gmail.com>
2012-11-07 15:14:22 +0530

    Initial Commit

---
+++ Cloudy/Cloudy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cloudy
+//
+//  Created by Parag Dulam on 07/11/12.
+//  Copyright (c) 2012 Parag Dulam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cloudy/Cloudy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cloudy
+//
+//  Created by Parag Dulam on 07/11/12.
+//  Copyright (c) 2012 Parag Dulam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
